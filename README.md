b47156074de957f3ec04027fc82501f8c5f1fd40
Andrew Esteves <esteves.andrew@gmail.com>
2017-06-28 13:32:21 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Digital Nativa](http://www.digitalnativa.com.br/img/digital-nativa.jpg)](http://www.digitalnativa.com.br)
+
+# Admin theme for web apps
+
+Simplest starter theme for web apps
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Digital Nativa | UI</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/app.css">
+    </head>
+    <body>
+
+    <div id="nav" class="sidenav nav-opened">
+        <div class="sidenav-wrapper">
+            <a href="#"><i class="fa fa-tachometer"></i> Dashboard</a>
+            <a href="#"><i class="fa fa-area-chart"></i> Relatórios</a>
+        </div>
+    </div>
+
+    <div id="main">
+        <span id="nav-toggle">Abrir</span>
+    </div>
+
+    <script type="text/javascript" src="assets/js/jquery.slim.min.js"></script>
+    <script type="text/javascript" src="assets/js/teather.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+    <script type="text/javascript" src="assets/js/app.js"></script>
+    </body>
+</html>
